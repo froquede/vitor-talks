f0ccef052d7c287361f5fef2908041bf07a41a8c
Roque Francisco <roquefsd@gmail.com>
2018-05-17 02:17:24 -0300

    simple joystick over bluetooth

---
+++ arduino/joystick/joystick.ino
@@ -0,0 +1,35 @@
+int left_trigger = 0;
+int left_switch = 5;
+int up_trigger = 0;
+int up_switch = 6;
+int right_trigger = 0;
+int right_switch = 7;
+int down_trigger = 0;
+int down_switch = 8;
+
+void setup() {
+  pinMode(left_switch, INPUT);     
+  Serial.begin(9600);
+}
+
+void loop(){
+  left_trigger = digitalRead(left_switch);
+  if(left_trigger == HIGH){
+     Serial.println('left');
+  }
+
+  up_trigger = digitalRead(up_switch);
+  if(up_trigger == HIGH){
+     Serial.println('up');
+  }
+
+  right_trigger = digitalRead(right_switch);
+  if(right_trigger == HIGH){
+     Serial.println('right');
+  }
+
+  down_trigger = digitalRead(down_switch);
+  if(down_trigger == HIGH){
+     Serial.println('down');
+  }
+}
--- arduino/serial-mode-hc-06/serial-mode-hc-06.ino
+++ arduino/serial-mode-hc-06/serial-mode-hc-06.ino
@@ -1,19 +1,27 @@
 #include <SoftwareSerial.h>
+SoftwareSerial mySerial(10, 11); // RX, TX
 
-SoftwareSerial mySerial (2,3); // RX, TX
+void setup(){
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  mySerial.begin(38400);
 
-void setup () {
-    mySerial.begin(115200);
-    Serial.begin(115200);
-    Serial.println("Enter AT commands:");
+  mySerial.print("AT");
+  delay(500);
+  mySerial.print("AT+NAMEvitortalks");
+  delay(500);
+  mySerial.print("AT+PIN0000");
+  delay(500);
 }
 
-void loop() {
-    while (mySerial.available()) {
-        Serial.write(mySerial.read());
-    }
+void loop(){
+  if (mySerial.available()){
+    Serial.println("connected");
+    Serial.write(mySerial.read());
+  }
+  if (Serial.available()){
+    mySerial.write(Serial.read());  
+  }
 
-    while (Serial.available()) {
-        mySerial.write(Serial.read());
-    }
+  delay(100);
 }
