b67da99cd9db7a9fb439ad4a43a7ba9503aa8b5d
Roque Francisco <roquefsd@gmail.com>
2018-05-19 15:04:25 -0300

    invert buttons

--- hw_index.js
+++ hw_index.js
@@ -3,15 +3,15 @@ var board = new five.Board();
 var left_switch, up_switch, right_switch, down_switch;
 var left_key, up_key, right_key, down_key;
 var io = require("socket.io")(81);
-var express = require('express');
+var express = require("express");
 var app = express();
 var http = require("http").Server(app);
 
 board.on("ready", function() {
-  left_switch = new five.Button({pin: 7, inverted: true});
-  up_switch = new five.Button({pin: 8, inverted: true});
-  right_switch = new five.Button({pin: 12, inverted: true});
-  down_switch = new five.Button({pin: 13, inverted: true});
+  left_switch = new five.Button({ pin: 7, invert: true });
+  up_switch = new five.Button({ pin: 8, invert: true });
+  right_switch = new five.Button({ pin: 12, invert: true });
+  down_switch = new five.Button({ pin: 13, invert: true });
 
   bindEvents(left_switch, "left");
   bindEvents(up_switch, "up");
@@ -28,7 +28,7 @@ board.on("ready", function() {
   });
 
   potentiometer.on("data", function() {
-	  io.emit("potentiometer", { value: this.value });
+    io.emit("potentiometer", { value: this.value });
   });
 });
 
@@ -45,7 +45,7 @@ function bindEvents(sw, key) {
     });
 }
 
-app.use(express.static('public'));	
+app.use(express.static("public"));
 http.listen(3000, function() {
   console.log("listening on *:3000");
 });
